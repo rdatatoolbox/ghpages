ab699c59d4ba1c7ddd4ccf733e4d879fc8dddd76
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-11-29 19:25:37 +0100

    Init repo

---
+++ make.R
@@ -0,0 +1,29 @@
+#' ghpages: Deploy HTML Files on GitHub Pages
+#' 
+#' @description 
+#' Example to show how to deploy HTML files on GitHub Pages.
+#' 
+#' @author Nicolas Casajus \email{nicolas.casajus@fondationbiodiversite.fr}
+#' 
+#' @date 2022/11/29
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+remotes::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+pkgload::load_all()
+
+
+## Global Variables ----
+
+# ...
+
+
+## Run Project ----
+
+# ...
